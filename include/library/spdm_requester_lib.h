5188ea41b3a9fbf5c7b64c0f62b411f8d4178d0c
Aaron Li <aaron.li@intel.com>
2026-02-27 10:45:25 +0800

    fix libspdm_get_key_pair_info_with_pqc and libspdm_set_key_pair_info_pqc visibility
    fix #3507
    
    Signed-off-by: Aaron Li <aaron.li@intel.com>

--- include/library/spdm_requester_lib.h
+++ include/library/spdm_requester_lib.h
@@ -438,6 +438,43 @@ libspdm_return_t libspdm_get_key_pair_info(void *spdm_context, const uint32_t *s
                                            uint16_t *public_key_info_len,
                                            void *public_key_info
                                            );
+
+/**
+ * This function sends GET_KEY_PAIR_INFO to get key pair info from device,
+ * and allows the caller to retrieve PQC algorithm capabilities and PQC algorithm currently in use.
+ *
+ * @param  spdm_context               A pointer to the SPDM context.
+ * @param  session_id                 Indicates if it is a secured message protected via SPDM session.
+ *                                    If session_id is NULL, it is a normal message.
+ *                                    If session_id is not NULL, it is a secured message.
+ * @param  key_pair_id                Indicate which key pair ID's information to retrieve.
+ *
+ * @param  total_key_pairs            Indicate the total number of key pairs on the responder.
+ * @param  capabilities               Indicate the capabilities of the requested key pairs.
+ * @param  key_usage_capabilities     Indicate the key usages the responder allows.
+ * @param  current_key_usage          Indicate the currently configured key usage for the requested key pairs ID.
+ * @param  asym_algo_capabilities     Indicate the asymmetric algorithms the Responder supports for this key pair ID.
+ * @param  current_asym_algo          Indicate the currently configured asymmetric algorithm for this key pair ID.
+ * @param  pqc_asym_algo_capabilities Indicate the PQC asymmetric algorithms the Responder supports for this key pair ID.
+ * @param  current_pqc_asym_algo      Indicate the currently configured PQC asymmetric algorithm for this key pair ID.
+ * @param  assoc_cert_slot_mask       This field is a bit mask representing the currently associated certificate slots.
+ * @param  public_key_info_len        On input, indicate the size in bytes of the destination buffer to store.
+ *                                    On output, indicate the size in bytes of the public_key_info.
+ * @param  public_key_info            A pointer to a destination buffer to store the public_key_info.
+ **/
+libspdm_return_t libspdm_get_key_pair_info_with_pqc(void *spdm_context, const uint32_t *session_id,
+                                                    uint8_t key_pair_id, uint8_t *total_key_pairs,
+                                                    uint16_t *capabilities,
+                                                    uint16_t *key_usage_capabilities,
+                                                    uint16_t *current_key_usage,
+                                                    uint32_t *asym_algo_capabilities,
+                                                    uint32_t *current_asym_algo,
+                                                    uint32_t *pqc_asym_algo_capabilities,
+                                                    uint32_t *current_pqc_asym_algo,
+                                                    uint8_t *assoc_cert_slot_mask,
+                                                    uint16_t *public_key_info_len,
+                                                    void *public_key_info
+                                                    );
 #endif /* LIBSPDM_ENABLE_CAPABILITY_GET_KEY_PAIR_INFO_CAP */
 
 #if LIBSPDM_ENABLE_CAPABILITY_SET_KEY_PAIR_INFO_CAP
@@ -461,6 +498,30 @@ libspdm_return_t libspdm_set_key_pair_info(void *spdm_context, const uint32_t *s
                                            uint32_t desired_asym_algo,
                                            uint8_t desired_assoc_cert_slot_mask
                                            );
+
+/**
+ * This function sends GET_KEY_PAIR_INFO to get key pair info from device,
+ * and allows the caller to set PQC algorithm for the key pair.
+ *
+ * @param  spdm_context                 A pointer to the SPDM context.
+ * @param  session_id                   Indicates if it is a secured message protected via SPDM session.
+ *                                      If session_id is NULL, it is a normal message.
+ *                                      If session_id is not NULL, it is a secured message.
+ * @param  key_pair_id                  Indicate which key pair ID's information to retrieve.
+ * @param  operation                    Set key pair info operation: change/erase/generate.
+ * @param  desired_key_usage            Indicate the desired key usage for the requested key pair ID.
+ * @param  desired_asym_algo            Indicate the desired asymmetric algorithm for the requested key pair ID.
+ * @param  desired_pqc_asym_algo        Indicate the desired PQC asymmetric algorithm for the requested key pair ID.
+ * @param  desired_assoc_cert_slot_mask Indicate the desired certificate slot association for the requested key pair ID.
+ **/
+libspdm_return_t libspdm_set_key_pair_info_pqc(void *spdm_context, const uint32_t *session_id,
+                                               uint8_t key_pair_id,
+                                               uint8_t operation,
+                                               uint16_t desired_key_usage,
+                                               uint32_t desired_asym_algo,
+                                               uint32_t desired_pqc_asym_algo,
+                                               uint8_t desired_assoc_cert_slot_mask
+                                               );
 #endif /* LIBSPDM_ENABLE_CAPABILITY_SET_KEY_PAIR_INFO_CAP */
 
 #if (LIBSPDM_ENABLE_CAPABILITY_KEY_EX_CAP) || (LIBSPDM_ENABLE_CAPABILITY_PSK_CAP)
